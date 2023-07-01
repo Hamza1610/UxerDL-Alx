70e27f34af65122dbba9e429cd4122925069e452
Hamza1060 <muhammadhamza162003@gmail.com>
2023-07-01 13:14:03 -0700

    added not found style

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,7 +1,7 @@
 const NotFound = () => {
     return (
         <div className="Not-found-div">
-            <h1>404</h1>
+            <h1 className="not-found-header">404</h1>
             <p>Page Not Found</p>
             <small>Url not correct</small>
         </div>
