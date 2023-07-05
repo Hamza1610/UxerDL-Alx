735642b75359476d9a2a9581bf931762b7c1bc8a
Hamza1060 <muhammadhamza162003@gmail.com>
2023-07-05 05:56:33 -0700

    preloader componenrt added

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
+
 import Card from "./card";
+import Preloader from './preloader';
 
 const List = () => {
 
     const [lists, setLists] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+          }, 2000);
         ListsFetch();
         console.log("Hellow");
     },[])
@@ -22,17 +28,23 @@ const List = () => {
 
     return (
         <div>
-            <h1>Lists of Books</h1>
-            <div className="list-div">
-                {/* List component fetch books detail from the database */}
+            {loading ? (
+                <Preloader />
+                ) : (
+                <div>
+                    <h1>Lists of Books</h1>
+                    <div className="list-div">
+                    {/* List component fetch books detail from the database */}
 
-                <div className="listRow">
-                    {lists && lists.map((item) => (
-                        <Card key={item._id} list={item} error={item.error}/>
-                    ))}
+                    <div className="listRow">
+                        {lists && lists.map((item) => (
+                            <Card key={item._id} list={item} error={item.error}/>
+                        ))}
+                    </div>
+                    <div><p>{lists.error}</p></div>
                 </div>
-                <div><p>{lists.error}</p></div>
             </div>
+                )}
         </div>
     )
 }
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import Card from "./card";
+import Preloader from './preloader'
 
 const Search = () => {
   const [data, setData] = useState([]);
   const [searchedFile, setSearchedFile] = useState('');
+  const [loading, setLoading] = useState(true);
 
   // Function to handle the API request and update the data state
   const handleSearch = async (e) => {
     e.preventDefault(); // Prevent the default form submission
-
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
     try {
       const response = await fetch('/api/search', {
         method: 'POST',
@@ -26,6 +30,7 @@ const Search = () => {
       const json = await response.json();
       console.log(json);
       setData(json); // Update the data state with the fetched results
+
     } catch (error) {
       console.error('Error:', error);
     }
@@ -33,27 +38,35 @@ const Search = () => {
 
   return (
     <div className="search-div">
-      <form className="searchForm" onSubmit={handleSearch}>
-        <input
-          type="search"
-          name="searchedFile"
-          placeholder="Search books here"
-          onChange={(e) => setSearchedFile(e.target.value)}
-          value={searchedFile}
-          className="searchInput"
-          required
-        />
 
-        <button className="searchButton" type="submit">
-          <b>Search</b>
-        </button>
-      </form>
       <div>
-        <h1>Search Results</h1>
-        <div className="search-viewer-div">
-          {data.map((item) => (
-            <Card key={item._id} list={item} error={item.error} />
-            )) || <p>No search results</p>}
+        <form className="searchForm" onSubmit={handleSearch}>
+          <input
+            type="search"
+            name="searchedFile"
+            placeholder="Search books here"
+            onChange={(e) => setSearchedFile(e.target.value)}
+            value={searchedFile}
+            className="searchInput"
+            required
+          />
+
+          <button className="searchButton" type="submit">
+            <b>Search</b>
+          </button>
+        </form>
+        <div>
+          <h1>Search Results</h1>
+          {loading ? (
+            <Preloader />
+            ) : (
+              <div className="search-viewer-div">
+                {data.map((item) => (
+                  <Card key={item._id} list={item} error={item.error} />
+                  )) || <p>No search results</p>
+                }
+              </div>
+            )}
         </div>
       </div>
     </div>
