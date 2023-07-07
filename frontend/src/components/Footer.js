a3daec52655a5a73713061df2ab22825e9e222b7
Hamza1060 <muhammadhamza162003@gmail.com>
2023-07-07 03:47:23 -0700

    icons added folder /icons

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
+
+import emailIcon from './icons/envelope-solid.svg';
+import linkedinIcon from './icons/linkedin-in.svg';
+import facebookIcon from './icons/facebook-f.svg';
+import twitterIcon from './icons/twitter.svg';
+import homeIcon from './icons/house-solid.svg';
+import booksIcon from './icons/book-solid.svg';
+import searchIcon from './icons/square-twitter.svg';
+import uploadIcon from './icons/upload-solid.svg';
+
 const Footer = () => {
     return (
     <footer>
         <div className="footer-contact-div">
             <h2>Contact Me</h2>
             <div className="contact-contents">
-                <Link to='' ><p>Email <i className=""></i></p></Link>
-                <Link to='' ><p>LinkedIn <i className=""></i></p></Link>
-                <Link to='' ><p>Twitter <i className=""></i></p></Link>
-                <Link to='' ><p>Facebook <i className=""></i></p></Link>
+                <Link to='' ><p>Email <img className="icon" src={emailIcon} alt=""/></p></Link>
+                <Link to='' ><p>LinkedIn <img className="icon" src={linkedinIcon} alt=""/></p></Link>
+                <Link to='' ><p>Twitter <img className="icon" src={twitterIcon} alt=""/></p></Link>
+                <Link to='' ><p>Facebook <img className="icon" src={facebookIcon} alt=""/></p></Link>
             </div>
         </div>
         <div className="footer-about-div">
@@ -25,10 +35,10 @@ const Footer = () => {
         <div className="footer-navigation-div">
             <h2>Navigation</h2>
             <nav className="navigation-contents">
-                <p><Link to='/'>Home</Link></p>
-                <p><Link to='/api/'>Books</Link></p>
-                <p><Link to='/api/search'>Search</Link></p>
-                <p><Link to='/api/upload'>Upload</Link></p>
+                <p><Link to='/'>Home <img className="icon" src={homeIcon} alt=""/></Link></p>
+                <p><Link to='/api/'>Books <img className="icon" src={booksIcon} alt=""/></Link></p>
+                <p><Link to='/api/search'>Search <img className="icon" src={searchIcon} alt=""/></Link></p>
+                <p><Link to='/api/upload'>Upload <img className="icon" src={uploadIcon} alt=""/></Link></p>
             </nav>
         </div>
         <div className="">
